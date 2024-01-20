24b08c0864709550a2a051c3f1ef91bac2d1b614
Crux Cook <cruxcook@tutanota.com>
2024-01-19 17:56:59 -0800

    feat(homepage): render content
    Issue #1
    Reference:
    - [Async JS](https://www.w3schools.com/js/js_async.asp)

--- app.js
+++ app.js
@@ -12,4 +12,55 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
   .then((data) => {
     allPokemons = data.results;
     console.log(allPokemons);
+    renderContent(allPokemons);
   });
+
+async function fetchDataBeforeRedirect(id) {
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => {
+        res.json();
+      }),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => {
+        res.json();
+      }),
+    ]);
+    return true;
+  } catch (error) {
+    console.log("Failed to fetch Pokemon data be4 redirect");
+  }
+}
+
+function renderContent(pokemon) {
+  listWrapper.innerHTML = "";
+
+  pokemon.forEach((p) => {
+    const pokemonId = p.url.split("/")[6];
+    const listItem = document.createElement("div");
+    listItem.className = "list-item";
+    listItem.innerHTML = `
+      <div class="number-wrap">
+        <p class="caption-fonts">#${pokemonId}</p>
+      </div>
+      <div class="img-wrap">
+        <img 
+          src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg" 
+          alt="${p.name}
+        />
+      </div>
+      <div class="name-wrap">
+        <p class="body3-fonts">#${p.name}</p>
+      </div>
+    `;
+
+    listItem.addEventListener("click", async () => {
+      const success = await fetchDataBeforeRedirect(pokemonId);
+
+      if (success) {
+        window.location.href = `./detail.html?id=${pokemonId}`;
+      }
+    });
+
+    listWrapper.appendChild(listItem);
+  });
+}
